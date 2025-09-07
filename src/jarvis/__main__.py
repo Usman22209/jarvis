d532ccbab4b9f0d1ab8e759ee03e341cbfff3f2a
Usman22209 <166890135+Usman22209@users.noreply.github.com>
2025-09-07 15:32:23 +0500

    customizedTts

--- src/jarvis/__main__.py
+++ src/jarvis/__main__.py
@@ -2,8 +2,8 @@ from jarvis.tts import speak
 
 def main():
     message = "Hello, I am Jarvis. How can I help you?"
-    print(message)   # Prints in terminal
-    speak(message)   # Speaks out loud
+    print(message)   
+    speak(message)  
 
 if __name__ == "__main__":
     main()
--- src/jarvis/tts.py
+++ src/jarvis/tts.py
@@ -1,7 +1,17 @@
 import pyttsx3
-
-def speak(text: str):
-    """Convert text to speech."""
+def list_voices():
     engine = pyttsx3.init()
+    voices = engine.getProperty("voices")
+    for i, voice in enumerate(voices):
+        print(f"[{i}] {voice.name} ({voice.id})")
+def speak(text: str):
+    engine = pyttsx3.init()
+    rate=engine.getProperty("rate")
+    voices=engine.getProperty("voices")
+    volume=engine.getProperty("volume")
+    engine.setProperty("rate", 220) 
+    engine.setProperty("voice", voices[1].id) 
+    list_voices()
+    print(rate,volume)
     engine.say(text)
     engine.runAndWait()
