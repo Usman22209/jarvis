df436c66c3d7ccc4ae8c8c46debfe20de570d11f
Usman22209 <166890135+Usman22209@users.noreply.github.com>
2025-09-07 22:09:48 +0500

    basic conversation

--- src/jarvis/__main__.py
+++ src/jarvis/__main__.py
@@ -1,11 +1,27 @@
-from jarvis.stt import listen
 from jarvis.tts import speak
+from jarvis.stt import listen
+import webbrowser
 
 def main():
-    speak("Hello, I am Jarvis. What should I do?")
-    command = listen()
-    print(f"User said: {command}")
-    speak(f"You said: {command}")
+    speak("Hello, I am Jarvis. Say my name to activate me. Say 'stop' anytime to quit.")
+
+    while True:
+        command = listen().lower()
+        print(f"User said: {command}")
+        if "stop" in command:
+            speak("Goodbye! Shutting down.")
+            break
+
+        if "jarvis" in command:
+            speak("Yes, I am listening. What’s your command?")
+            task = listen().lower()
+            print(f"Command: {task}")
+            if "stop" in task:
+                speak("Goodbye! Shutting down.")
+                break
+            else:
+                speak(f"You said: {task}")
+
 
 if __name__ == "__main__":
     main()
